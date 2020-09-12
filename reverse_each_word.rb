1456c8c349fe8d340d2c6ca6e9415c79d854d44c
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-12 20:53:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 #require 'pry'
 
 def reverse_each_word(sentence)
-  new_array = []
-  sentence_array = reverse_each_word.split
+  reverse_array = []
+  split_sentence = reverse_each_word.split
   
   reverse_each_word.each do |reverse|
     
