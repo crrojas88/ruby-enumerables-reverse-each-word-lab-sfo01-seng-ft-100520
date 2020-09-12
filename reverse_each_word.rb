c62a75b527c7bcd68402ae1fcfbf2809e6266f92
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-12 20:53:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(sentence)
   
   reverse_each_word.each do |reverse|
     
-    new_array << reverse.reverse
+    reverse_array << reverse.reverse.join
     
     end
 sentence_array
