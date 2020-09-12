bea5b56193342ff9607a5048a03c46b43e1a6045
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-12 20:58:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   reverse_array = []
   split_sentence = reverse_each_word.split
   
-  reverse_each_word.each do |element|
+  split_sentence.each do |element|
     
     reverse_array << element.reverse!.join("")
     
